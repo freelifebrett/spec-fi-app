92661e6c6b110e029e36a0fc7ed9548c443b1dff
brettmwright <brett@brettmwright.com>
2023-11-10 12:40:52 -0500

    Updated stepone to use material

--- src/components/MultiStepForm/FormSteps/StepOne/StepOne.js
+++ src/components/MultiStepForm/FormSteps/StepOne/StepOne.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateField, updateCurrentStep } from '../../../../redux/form/formSlice';
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
 
 const StepOne = () => {
   const dispatch = useDispatch();
@@ -25,7 +26,6 @@ const StepOne = () => {
     setErrors({ ...errors, [name]: errorMessage });
     dispatch(updateField({ fieldName: name, fieldValue: value }));
   };
-  
 
   const handleSubmit = () => {
     // Validate form data, etc.
@@ -33,49 +33,62 @@ const StepOne = () => {
     dispatch(updateCurrentStep(2));
   };
 
-
   // Check if the form can proceed to the next step
   const canProceed = !errors.firstName && !errors.lastName && formData.firstName && formData.lastName;
 
   return (
-    <div>
-      <h2>Step 1: Personal Information</h2>
-      <form>
-        <label htmlFor="firstName">First Name (Required):</label>
-        <input
-          type="text"
+    <Container maxWidth="sm">
+      <Typography variant="h4" component="h2" gutterBottom>
+        Step 1: Personal Information
+      </Typography>
+      <Box component="form" noValidate autoComplete="off">
+        <TextField
+          fullWidth
           id="firstName"
           name="firstName"
+          label="First Name (Required)"
           value={formData.firstName || ''}
           onChange={handleFieldChange}
+          error={!!errors.firstName}
+          helperText={errors.firstName}
+          margin="normal"
           required
         />
-        {errors.firstName && <p className="error">{errors.firstName}</p>}
 
-        <label htmlFor="middleName">Middle Name:</label>
-        <input
-          type="text"
+        <TextField
+          fullWidth
           id="middleName"
           name="middleName"
+          label="Middle Name"
           value={formData.middleName || ''}
           onChange={handleFieldChange}
+          margin="normal"
         />
 
-        <label htmlFor="lastName">Last Name (Required):</label>
-        <input
-          type="text"
+        <TextField
+          fullWidth
           id="lastName"
           name="lastName"
+          label="Last Name (Required)"
           value={formData.lastName || ''}
           onChange={handleFieldChange}
+          error={!!errors.lastName}
+          helperText={errors.lastName}
+          margin="normal"
           required
         />
-        {errors.lastName && <p className="error">{errors.lastName}</p>}
 
-        {/* Navigation buttons or links to move between steps */}
-        <button type="button" onClick={handleSubmit} disabled={!canProceed}>Next</button>
-      </form>
-    </div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+          disabled={!canProceed}
+          sx={{ mt: 2 }}
+        >
+          Next
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
