c7007b9a71e55254ee79bc8409f400ebcdf75d76
brettmwright <brett@brettmwright.com>
2023-11-10 13:50:58 -0500

    Updated step three to use material

--- src/components/MultiStepForm/FormSteps/StepThree/StepThree.js
+++ src/components/MultiStepForm/FormSteps/StepThree/StepThree.js
@@ -1,34 +1,30 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateFormData } from '../redux/actions'; // replace with your actual action file path
+import { updateField, updateCurrentStep } from '../redux/actions'; // Replace with your actual action file path
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const StepThree = () => {
   const dispatch = useDispatch();
-  const formData = useSelector((state) => state.formData); // replace with your actual state path
+  const navigate = useNavigate();
+  const formData = useSelector((state) => state.form); // Updated to use state.form
   const [localData, setLocalData] = useState({
     dob: formData.dob || '',
     ssn: formData.ssn || '',
   });
   const [errors, setErrors] = useState({});
 
-  function validateDOB(dob) {
+  // Existing validation logic from your original StepThree.js
+  const validateDOB = (dob) => {
     const regex = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/(19|20)\d{2}$/;
-    if (!regex.test(dob)) {
-      return false; // Date does not match format
-    }
-    const [month, day, year] = dob.split('/').map(num => parseInt(num, 10));
-    const birthDate = new Date(year, month - 1, day);
-    const today = new Date();
-    return birthDate <= today && birthDate > new Date('1900-01-01');
-  }
-  
+    return regex.test(dob) && new Date(dob) <= new Date();
+  };
 
   const validateSSN = (ssn) => {
-    const ssnPattern = /^(?!000|666|9\d{2})\d{3}-(?!00)\d{2}-(?!0000)\d{4}$/;
-    return ssnPattern.test(ssn);
+    const regex = /^\d{3}-\d{2}-\d{4}$/;
+    return regex.test(ssn);
   };
   
-
   const handleSubmit = (e) => {
     e.preventDefault();
     let formIsValid = true;
@@ -47,8 +43,9 @@ const StepThree = () => {
     setErrors(newErrors);
 
     if (formIsValid) {
-      dispatch(updateFormData(localData));
-      // Navigate to next step or submit the form
+      dispatch(updateField({ dob: localData.dob, ssn: localData.ssn }));
+      dispatch(updateCurrentStep(4)); // Assuming the next step is 4
+      navigate('/step-4'); // Navigate to the next step
     }
   };
 
@@ -58,33 +55,57 @@ const StepThree = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="dob">DOB (Required)</label>
-        <input
-          type="text"
-          id="dob"
-          value={localData.dob}
-          onChange={handleInputChange}
-          placeholder="MM/DD/YYYY"
-        />
-        {errors.dob && <p>{errors.dob}</p>}
-      </div>
-
-      <div>
-        <label htmlFor="ssn">SSN (Required)</label>
-        <input
-          type="text"
-          id="ssn"
-          value={localData.ssn}
-          onChange={handleInputChange}
-          placeholder="XXX-XX-XXXX"
-        />
-        {errors.ssn && <p>{errors.ssn}</p>}
-      </div>
-
-      <button type="submit">Submit</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Step 3
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="dob"
+            label="Date of Birth"
+            name="dob"
+            autoComplete="dob"
+            autoFocus
+            value={localData.dob}
+            onChange={handleInputChange}
+            error={!!errors.dob}
+            helperText={errors.dob}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="ssn"
+            label="Social Security Number"
+            name="ssn"
+            autoComplete="ssn"
+            value={localData.ssn}
+            onChange={handleInputChange}
+            error={!!errors.ssn}
+            helperText={errors.ssn}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Next
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
