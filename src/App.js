7ee96a7cdb1b4bf6677da2f6f6b392a398113629
brettmwright <brett@brettmwright.com>
2023-11-11 18:06:29 -0500

    Added firebase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import store from './redux/store';
 import MultiStepForm from './components/MultiStepForm/MultiStepForm.js';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjzrtyLcd_HLIonYrEV69maYVqoCB33Qc",
+  authDomain: "spec-fi-app.firebaseapp.com",
+  projectId: "spec-fi-app",
+  storageBucket: "spec-fi-app.appspot.com",
+  messagingSenderId: "900568801328",
+  appId: "1:900568801328:web:cc138bb9d768f4a6e3cdf5",
+  measurementId: "G-XD6B0LMEZX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <Provider store={store}>
