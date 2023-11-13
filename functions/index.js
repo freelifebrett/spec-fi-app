dc8aa3421d332a815ab89957ff4a8ce6875323fe
brettmwright <brett@brettmwright.com>
2023-11-13 10:09:35 -0500

    Updates

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const js2xmlparser = require("js2xmlparser");
+const axios = require("axios");
 const cors = require('cors')({ origin: true });
 admin.initializeApp();
 
